6454140af731f6e80fb556e3c3868df67b085b30
lds <000000@163.com>
2025-03-12 14:42:06 +0800

    1

--- 1_2Msg/main.cpp
+++ 1_2Msg/main.cpp
@@ -1,33 +1,44 @@
 #include <afxwin.h>
+
 class CMyFrameWnd : public CFrameWnd {
-	//	DECLARE_MESSAGE_MAP()
-protected:
-	static const AFX_MSGMAP* PASCAL GetThisMessageMap();
-	virtual const AFX_MSGMAP* GetMessageMap() const;
+	DECLARE_MESSAGE_MAP()
+//protected:
+//	static const AFX_MSGMAP* PASCAL GetThisMessageMap();
+//	virtual const AFX_MSGMAP* GetMessageMap() const;
 public:
 	LRESULT OnCreate(WPARAM wParam, LPARAM lParam);
+	LRESULT OnPaint(WPARAM wParam, LPARAM lParam);
 };
-//BEGIN_MESSAGE_MAP(CMyFrameWnd, CFrameWnd)
-//	ON_MESSAGE( WM_CREATE, OnCreate )
-//END_MESSAGE_MAP()
 
+BEGIN_MESSAGE_MAP(CMyFrameWnd, CFrameWnd)
+	ON_MESSAGE(WM_PAINT, OnPaint)
+	ON_MESSAGE( WM_CREATE, OnCreate )
+END_MESSAGE_MAP()
 
-const AFX_MSGMAP* CMyFrameWnd::GetMessageMap() const
-{
-	return GetThisMessageMap();
-}
-const AFX_MSGMAP* PASCAL CMyFrameWnd::GetThisMessageMap()
-{
-	static const AFX_MSGMAP_ENTRY _messageEntries[] =
-	{
-//-------------------------------------------------------------------------------------------
-		{ WM_CREATE, 0, 0, 0, AfxSig_lwl, (AFX_PMSG)(AFX_PMSGW)
-			(static_cast<LRESULT(AFX_MSG_CALL CWnd::*)(WPARAM, LPARAM)> (&OnCreate)) },
-//---------------------------------------------------------------------------------------
-		{0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }
-	};
-	static const AFX_MSGMAP messageMap = { &CFrameWnd::GetThisMessageMap, &_messageEntries[0] };
-	return &messageMap;
+//const AFX_MSGMAP* CMyFrameWnd::GetMessageMap() const
+//{
+//	return GetThisMessageMap();
+//}
+//const AFX_MSGMAP* PASCAL CMyFrameWnd::GetThisMessageMap()
+//{
+//	static const AFX_MSGMAP_ENTRY _messageEntries[] =
+//	{
+////-------------------------------------------------------------------------------------------
+//		{ WM_CREATE, 0, 0, 0, AfxSig_lwl, (AFX_PMSG)(AFX_PMSGW)
+//			(static_cast<LRESULT(AFX_MSG_CALL CWnd::*)(WPARAM, LPARAM)> (&OnCreate)) },
+////---------------------------------------------------------------------------------------
+//		{0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }
+//	};
+//	static const AFX_MSGMAP messageMap = { &CFrameWnd::GetThisMessageMap, &_messageEntries[0] };
+//	return &messageMap;
+//}
+
+LRESULT CMyFrameWnd::OnPaint(WPARAM wParam, LPARAM lParam) {
+	PAINTSTRUCT ps = { 0 };
+	HDC hdc = ::BeginPaint(this->m_hWnd, &ps);
+	::TextOut(hdc, 100, 100, "hello", 5);
+	::EndPaint(m_hWnd, &ps);
+	return 0;
 }
 
 LRESULT CMyFrameWnd::OnCreate(WPARAM wParam, LPARAM lParam) {
---
+++ 1_2Msg_1/main.cpp
@@ -0,0 +1,54 @@
+#include <afxwin.h>
+#define WM_MYMESSAGE WM_USER+1001
+class CMyFrameWnd : public CFrameWnd {
+	DECLARE_MESSAGE_MAP()
+public:
+	int OnCreate(LPCREATESTRUCT pcs);
+	void OnPaint();
+	void OnMouseMove(UINT nKey, CPoint pt);
+	LRESULT OnMyMessage(WPARAM wParam, LPARAM lParam);
+	int m_x;
+	int m_y;
+};
+BEGIN_MESSAGE_MAP(CMyFrameWnd, CFrameWnd)
+	ON_WM_CREATE()
+	ON_WM_PAINT()
+	ON_WM_MOUSEMOVE()
+	ON_MESSAGE(WM_MYMESSAGE, OnMyMessage)
+END_MESSAGE_MAP()
+
+LRESULT CMyFrameWnd::OnMyMessage(WPARAM wParam, LPARAM lParam) {
+	CString str;
+	str.Format("wParam=%d,lParam=%d", wParam, lParam);
+	AfxMessageBox(str);
+	return 0;
+}
+void CMyFrameWnd::OnMouseMove(UINT nKey, CPoint pt) {
+	m_x = pt.x;
+	m_y = pt.y;
+	::InvalidateRect(this->m_hWnd, NULL, TRUE);
+}
+void CMyFrameWnd::OnPaint() {
+	PAINTSTRUCT ps = { 0 };
+	HDC hdc = ::BeginPaint(this->m_hWnd, &ps);
+	::TextOut(hdc, m_x, m_y, "hello", 5);
+	::EndPaint(m_hWnd, &ps);
+}
+int CMyFrameWnd::OnCreate(LPCREATESTRUCT pcs) {
+	AfxMessageBox("WM_CREATE消息被处理");
+	::PostMessage(this->m_hWnd, WM_MYMESSAGE, 1, 2);
+	return CFrameWnd::OnCreate(pcs);
+}
+class CMyWinApp : public CWinApp {
+public:
+	virtual BOOL InitInstance();
+};
+CMyWinApp theApp;//爆破点
+BOOL CMyWinApp::InitInstance() {
+	CMyFrameWnd* pFrame = new CMyFrameWnd;
+	pFrame->Create(NULL, "MFCCreate");
+	m_pMainWnd = pFrame;
+	pFrame->ShowWindow(SW_SHOW);
+	pFrame->UpdateWindow();
+	return TRUE;
+}
